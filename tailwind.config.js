ee8370e455e7de784f95d8824e703688a584db55
zubair <muhammad.zubair.i.am@gmail.com>
2021-10-19 12:40:24 +0500

    version2 - improved tailwind usage

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        footerBG: "#041a35",
+        customGreen: "#5DB05A",
+        customYellow: "#F5E74F",
+        customBlue: "#315CA4",
+        grayBG: "#F1F1F1",
+        hrLineColor: "#C1C6CD",
+      },
+      /* backgroundImage: {
+        "webApp-bg": "url('/images/streetBoy.svg')",
+      }, */
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
